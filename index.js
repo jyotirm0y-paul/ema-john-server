0349166db1c2b002b94b6ef8ca5ca3076ffb397f
jyotirm0y-paul <jyotirmoooy@gmail.com>
2021-04-24 09:57:46 +0600

    again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config() 
+require('dotenv').config()
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2r2l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
